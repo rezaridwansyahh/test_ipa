c30113e6bdabdf36da85f52b78f5fce694088fce
Reza ridwansyah <rezaridwansyah10@gmail.com>
2017-05-11 17:09:39 +0700

    nambah search

---
+++ resources/views/layout/sidekiributton.blade.php
@@ -0,0 +1,6 @@
+<div class="membership" align="center">
+	<h3 style="margin-bottom: 10px">LOGIN</h3>
+	<hr>
+	<button class="btn btn-primary"><a href="/signinmember" style="color: white">CONVENTION LOGIN</a></button>
+</div>
+
